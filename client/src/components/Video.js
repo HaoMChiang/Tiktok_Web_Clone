d2865aaeb5bcec3650ba7f2ccf32a4325ac799b5
Hao Chiang <hmc@Haos-MacBook-Pro.local>
2021-06-27 00:19:53 -0700

    first commit

---
+++ client/src/components/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styling/Video.css'
+
+const Video = () => {
+    return (
+        <div className='video'>
+            
+        </div>
+    )
+}
+
+export default Video
